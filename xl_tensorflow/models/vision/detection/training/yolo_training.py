06bbfec00d20218a6a9e38aea715b68dccbd21bb
Lannister-Xiaolin <119xiaolin@163.com>
2020-06-04 14:13:55 +0800

    yolo数据增强参数更改为ignore0.4,iou scale=1, autoaugment_ratio=0.8,新增随机翻转

--- xl_tensorflow/models/vision/detection/training/yolo_training.py
+++ xl_tensorflow/models/vision/detection/training/yolo_training.py
@@ -22,14 +22,14 @@ def mul_gpu_training_custom_data(train_annotation_path, val_annotation_path,
                                  suffix="voc", pre_weights=None, anchors="v3",
                                  use_multiprocessing=False, workers=1,
                                  tfrecord=True,
-                                 lrs=(1e-5, 1e-3, 1e-4),
+                                 lrs=(1e-4, 5e-4, 5e-5),
                                  freeze_layers=(185, 185, 0),
-                                 epochs=(2, 30, 50), initial_epoch=0,
-                                 paciences=(10, 10, 5),
-                                 reduce_lrs=(3, 3, 3), trunc_inf=True,
+                                 epochs=(20, 40, 60), initial_epoch=0,
+                                 paciences=(10, 10, 8),
+                                 reduce_lrs=(5, 5, 5), trunc_inf=True,
                                  ignore_thresh=0.4, print_loss=True,
-                                 iou_scale=2.0,
-                                 autoaugment_policy_name="v1", autoaugment_ratio=0.8, buffer=1000,
+                                 iou_scale=1.0,
+                                 autoaugment_policy_name="v0", autoaugment_ratio=0.8, buffer=1000,
                                  aug_scale_max=1.2, aug_scale_min=0.8):
     """
 
@@ -110,12 +110,12 @@ def mul_gpu_training_custom_data(train_annotation_path, val_annotation_path,
     else:
         train_dataset = YoloInputFn(input_shape, train_annotation_path,
                                     num_classes, aug_scale_max=aug_scale_max, aug_scale_min=aug_scale_min,
-                                    use_autoaugment=True,aug_rand_hflip=True,
+                                    use_autoaugment=True, aug_rand_hflip=True,
                                     autoaugment_policy_name=autoaugment_policy_name, anchor=anchors,
                                     autoaugment_ratio=autoaugment_ratio, buffer=buffer)(batch_size=batch_size)
         val_dataset = YoloInputFn(input_shape, val_annotation_path,
                                   num_classes, anchor=anchors, aug_scale_max=1.0, aug_scale_min=1.0,
-                                  use_autoaugment=False,aug_rand_hflip=False)(
+                                  use_autoaugment=False, aug_rand_hflip=False)(
             batch_size=batch_size)
     for i in range(len(lrs)):
         if epochs[i] <= initial_epoch: continue
