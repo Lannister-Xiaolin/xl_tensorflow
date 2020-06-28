691df4e05d8e389a31a0749ca7d670d6790c1ac8
Lannister-Xiaolin <119xiaolin@163.com>
2020-06-28 14:15:26 +0800

    gradient clip

--- xl_tensorflow/models/vision/detection/dataloader/input_reader.py
+++ xl_tensorflow/models/vision/detection/dataloader/input_reader.py
@@ -185,7 +185,7 @@ class YoloInputFn(object):
             dataset = dataset.repeat(1)
 
         dataset = dataset.interleave(
-            map_func=self._dataset_fn, cycle_length=8,
+            map_func=self._dataset_fn, cycle_length=32,
             num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
         if self._is_training:
